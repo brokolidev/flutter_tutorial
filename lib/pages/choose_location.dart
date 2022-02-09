ed71598781fb67a2cf829f4a9f9686af3bec28b1
brokoli <bocalist@gmail.com>
2022-02-09 23:01:36 +0900

    widget lifecycle

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -8,17 +8,36 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
+
+  int counter = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    print('initState');
+  }
+
   @override
   Widget build(BuildContext context) {
+
+    print('build');
+
     return Scaffold(
       backgroundColor: Colors.grey[200],
       appBar: AppBar(
         backgroundColor: Colors.blue[900],
-        title: Text('Choose a Location'),
+        title: Text('Choose a Location!'),
         centerTitle: true,
         elevation: 0,
       ),
-      body: Text('Choose Location Screen'),
+      body: RaisedButton(
+        onPressed: () {
+          setState(() {
+            counter ++;
+          });
+        },
+        child: Text('counter is $counter'),
+      ),
     );
   }
 }
