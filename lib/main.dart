7f99a8e284d90c38bb006c96a957da96c7f30820
brokoli <bocalist@gmail.com>
2022-02-09 22:55:55 +0900

    기본 라우팅 개념

--- lib/main.dart
+++ lib/main.dart
@@ -1,47 +1,15 @@
 import 'package:flutter/material.dart';
-import 'member.dart';
-import 'member_card.dart';
+import 'package:ft_tutorial/pages/home.dart';
+import 'package:ft_tutorial/pages/loading.dart';
+import 'package:ft_tutorial/pages/choose_location.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: QuoteList(),
+    initialRoute: '/home',
+    routes: {
+      '/': (context) => Loading(),
+      '/home': (context) => Home(),
+      '/location': (contenxt) => ChooseLocation(),
+    },
   ));
-}
-
-class QuoteList extends StatefulWidget {
-  const QuoteList({Key? key}) : super(key: key);
-
-  @override
-  _QuoteListState createState() => _QuoteListState();
-}
-
-class _QuoteListState extends State<QuoteList> {
-
-  List<Member> members = [
-    Member(name: 'Lee', age: 37, sex: 'm'),
-    Member(name: 'Choi', age: 34, sex: 'm'),
-    Member(name: 'Jung', age: 27, sex: 'f'),
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[200],
-      appBar: AppBar(
-        title: Text('리스트 출력'),
-        centerTitle: true,
-        backgroundColor: Colors.redAccent,
-      ),
-      body: Column(
-        children: members.map((member) => MemberCard(
-          member: member,
-          delete: () {
-            setState(() {
-              members.remove(member);
-            });
-          }
-        )).toList(),
-      ),
-    );
-  }
 }
