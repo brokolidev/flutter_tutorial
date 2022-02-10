d51210358453b26a9d564fbd80ce486897368edf
brokoli <bocalist@gmail.com>
2022-02-10 13:48:30 +0900

    get world time api

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -11,18 +11,26 @@ class Loading extends StatefulWidget {
 
 class _LoadingState extends State<Loading> {
 
-  void getData() async {
+  void getTime() async {
 
     var url =
-    Uri.https('www.googleapis.com', '/books/v1/volumes', {'q': '{http}'});
+    Uri.http('worldtimeapi.org', '/api/timezone/Asia/Seoul');
 
     // Await the http get response, then decode the json-formatted response.
     var response = await http.get(url);
     if (response.statusCode == 200) {
-      var jsonResponse =
+      var timeData =
       convert.jsonDecode(response.body) as Map<String, dynamic>;
-      var itemCount = jsonResponse['totalItems'];
-      print('Number of books about http: $itemCount.');
+
+      String datetime = timeData['utc_datetime'];
+      String offset = timeData['utc_offset'].substring(1, 3);
+
+      DateTime now = DateTime.parse(datetime);
+
+      now = now.add(Duration(hours: int.parse(offset)));
+
+      print(now);
+
     } else {
       print('Request failed with status: ${response.statusCode}.');
     }
@@ -32,7 +40,7 @@ class _LoadingState extends State<Loading> {
   @override
   void initState() {
     super.initState();
-    getData();
+    getTime();
   }
 
   @override
